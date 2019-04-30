b770e6157f275ec0da986572e415b22318db1f67
Thomas W <wesselink.tc@gmail.com>
2019-04-30 09:41:04 +0200

    Added html and css formatting, changed readme

--- cvplayground.py
+++ cvplayground.py
@@ -1,10 +1,34 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
+
+
+@app.context_processor
+def override_url_for():
+    '''
+    Code to override url_for to expire css files. Tells browser to update style when
+    changes have been made to css
+    '''
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    '''
+    Attaches timestamp to url
+    '''
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
 # Computer Vision Playground
+Pet project to apply different computer vision concepts (such as face and gesture recognition) to live webcam stream on web application.
 
-## Introduction
-Pet project to apply different computer vision concepts to live webcam stream on web application
+## Usage
+1. Run the cvplayground.py script
+2. Navigate to the adress shown in the console e.g: `* Running on http://X.X.X.X:XXXX/`
+
+## Contributing
+You are free to contribute to the project, create an issue to let me know.
